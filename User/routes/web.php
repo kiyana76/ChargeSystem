4a3cc297a77550fccafe6e08dcf1bb06439ed068
kiyana kiyaee <kiyana76@gmail.com>
2021-09-14 11:21:40 +0430

    register admin and show user

--- User/routes/web.php
+++ User/routes/web.php
@@ -4,7 +4,8 @@
 
 
 $router->post('admin/login', 'UserAuthController@login');
-$router->post('register/seller', ['uses' =>'UserAuthController@registerSeller', 'middleware' => 'auth']);
+$router->post('register/user', ['uses' =>'UserAuthController@registerUser', 'middleware' => 'auth']);
+$router->get('user/{id}', 'UserController@show');
 
 $router->post('companies', 'CompanyController@create');
 $router->get('companies', 'CompanyController@index');
