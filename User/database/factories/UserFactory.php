638dfa1d35be102323534c685c3fe52f9066463d
kiyana kiyaee <kiyana76@gmail.com>
2021-09-15 17:36:03 +0430

    add seller and admin and show user test is ok

--- User/database/factories/UserFactory.php
+++ User/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -21,9 +22,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $status_arr = ['active', 'inactive'];
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'type' => $this->faker->randomElement(['admin', 'seller']),
+            'mobile' => $this->faker->numerify('09#######'),
+            'password' => Hash::make('123456'),
+            'status' => $status_arr[array_rand($status_arr)]
         ];
     }
 }
