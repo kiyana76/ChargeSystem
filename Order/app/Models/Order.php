2dc2d5f190afb886009ff3df7adccdd8885c54d1
kiyana kiyaee <kiyana76@gmail.com>
2021-10-09 18:11:54 +0330

    elastic config ok

--- Order/app/Models/Order.php
+++ Order/app/Models/Order.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Elasticquent\ElasticquentTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, ElasticquentTrait;
 
 
     protected $fillable = [
@@ -19,6 +16,51 @@ class Order extends Model
         'customer_id',
         'mobile'
     ];
+
+    /**
+     * The elasticsearch settings.
+     *
+     * @var array
+     */
+    protected $indexSettings = [
+        'analysis' => [
+            'char_filter' => [
+                'replace' => [
+                    'type' => 'mapping',
+                    'mappings' => [
+                        '&=> and '
+                    ],
+                ],
+            ],
+            'filter' => [
+                'word_delimiter' => [
+                    'type' => 'word_delimiter',
+                    'split_on_numerics' => false,
+                    'split_on_case_change' => true,
+                    'generate_word_parts' => true,
+                    'generate_number_parts' => true,
+                    'catenate_all' => true,
+                    'preserve_original' => true,
+                    'catenate_numbers' => true,
+                ]
+            ],
+            'analyzer' => [
+                'default' => [
+                    'type' => 'custom',
+                    'char_filter' => [
+                        'html_strip',
+                        'replace',
+                    ],
+                    'tokenizer' => 'whitespace',
+                    'filter' => [
+                        'lowercase',
+                        'word_delimiter',
+                    ],
+                ],
+            ],
+        ],
+    ];
+
 
     public function orderItem() {
         return $this->hasMany(OrderItem::class);
--- Order/routes/web.php
+++ Order/routes/web.php
@@ -9,5 +9,8 @@ $router->get('orders', 'OrderController@index');
 $router->get('orders/charge-details', 'OrderController@indexWithChargeDetails');
 
 $router->get('/', function () use ($router) {
+    \App\Models\Order::createIndex($shards = null, $replicas = null);
+    $orders = \App\Models\Order::with('orderItem')->get();
+    $orders->addToIndex();
     return $router->app->version();
 });
